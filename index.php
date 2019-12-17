d617c757cf69af897f4387dc340cc66b89a92da4
Zac Sliker <zasliker@gmail.com>
2019-12-17 13:27:45 -0800

    update to index.php

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ session_start();
 require_once 'header.php';
     $error = $user = $pass = "";
 echo<<<_END
-
+<h1> Zac Sliker </h1>
+    <p>Website in progress</p>
+    <div>
+        <img id = 'Zac' src='img/zac.jpg' alt='zac' height='300' weight='300'>
+    </div>
 _END;
 ?>

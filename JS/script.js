bf9b58c7ec478af3a4e5751e282fb5c55461d09f
Zac Sliker <zasliker@gmail.com>
2019-12-26 20:39:54 -0800

    testing tabs

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,13 @@
-function openPage(pageName, elmnt) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
+function selectTab(tabIndex) {
+  //Hide All Tabs
+  document.getElementById('Home').style.display="none";
+  document.getElementById('About').style.display="none";
+  document.getElementById('History').style.display="none";
+  document.getElementById('Hobbies').style.display="none";
+  document.getElementById('Awards').style.display="none";
+  document.getElementById('Resume').style.display="none";
+  
+  
+  //Show the Selected Tab
+  document.getElementById(tabIndex).style.display="block";  
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
